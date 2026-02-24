00f959ff5e0a2a79f6388de034de76f3a173597e
lgrmatheo-lgtm <lagiermatheo@gmail.com>
2026-02-24 16:50:30 +0100

    "Meilleur contraste des couleurs"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,7 @@ import { CartContext } from "../context/cartContext.jsx";
 import { SearchContext } from "../context/searchContext.jsx";
 import { getDiscountPercent, getDiscountedPrice } from "../utils/discounts.js";
 
-const heroImage = "https://images.unsplash.com/photo-1509042239860-f550ce710b93?auto=format&fit=crop&w=1600&q=80";
-
+const heroImage = "https://images.unsplash.com/photo-1582200371328-11d78e4cea8d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 const universItems = [
     {
         id: "cafe",
